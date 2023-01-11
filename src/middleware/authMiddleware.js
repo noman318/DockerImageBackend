0ab2d04dc542cafb821a842b9d134389fadeb1f8
mohdrazaneosoft <mohd.raza@neosoftmail.com>
2023-01-11 11:20:55 +0530

    Add auth

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt=require('jsonwebtoken');
+const jwtvalidate=()=>{
+    return (req,res,next)=>{
+        if(req.headers &&req.headers.authorization && req.headers.authorization.split(' ')[0]==='Bearer')
+        {
+            let token=req.headers.authorization.split(' ')[1];
+            jwt.verify(token,process.env.SECRET_KEY,(err,decode)=>{
+                if(err){
+                    res.send("wrong token")
+                }
+                else{
+                    next();
+                }
+            })
+        }
+        else{
+            res.status(400).json({
+                error:1,
+                msg:"Pls Provide token"
+            });
+        }
+    }
+}
+module.exports=jwtvalidate;
