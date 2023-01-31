d5b2a33c2c22f5ab83efd375a1415c128138deeb
bharatsuthar26 <sutharbharat921@gmail.com>
2023-01-31 15:14:26 +0530

    remove mistake

--- src/routes/myProfileRouter.js
+++ src/routes/myProfileRouter.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const { formValidation } = require("../middleware/formValidation");
 const {profileController } = require("../controller/myProfileController");
 router.put("/api/myprofile/update/:id",formValidation.myProfileValidationRules(),profileController.updateProfileById);
-router.get("/api/myprofile/:id",formValidation.myProfileValidationRules(),profileController.getProfileById);
+router.get("/api/myprofile/:id",profileController.getProfileById);
 module.exports = router;
